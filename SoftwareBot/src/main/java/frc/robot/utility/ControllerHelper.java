e6b57f46f22ca1572cff0820260a710596821f25
Will Tomasini <34931592+wltomasini@users.noreply.github.com>
2023-02-04 16:40:02 -0800

    Tune deadband

--- SoftwareBot/src/main/java/frc/robot/utility/ControllerHelper.java
+++ SoftwareBot/src/main/java/frc/robot/utility/ControllerHelper.java
@@ -4,7 +4,7 @@ import edu.wpi.first.math.MathUtil;
 
 public class ControllerHelper {
 
-    private static double deadbandWidth = 0.05;
+    private static double deadbandWidth = 0.02;
     
     /**
      * @param value - Raw value read from controller axis to be modified
@@ -12,6 +12,6 @@ public class ControllerHelper {
      */
     public static double modifyAxis(double value) {
         value = MathUtil.applyDeadband(value, deadbandWidth);
-        return Math.copySign(value * value, value);
+        return Math.copySign(value * value * value, value);
     }
 }
