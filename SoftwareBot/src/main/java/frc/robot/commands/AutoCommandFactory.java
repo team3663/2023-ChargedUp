97e8722eb3d1d07d511ca93f34fdd48742443629
Richard Elmore <richardelmore@live.com>
2023-02-23 00:16:55 -0800

    Remove redundant code from auto factory

--- SoftwareBot/src/main/java/frc/robot/commands/AutoCommandFactory.java
+++ SoftwareBot/src/main/java/frc/robot/commands/AutoCommandFactory.java
@@ -22,101 +22,61 @@ public final class AutoCommandFactory {
     private static final PIDConstants AUTO_ROTATION_PID_CONSTANTS = new PIDConstants(2.0, 0.0, 0.0);
 
     private static PathConstraints pathConstraints = new PathConstraints(1.0, 1.0);
-    
+
+    private static HashMap<String, Command> eventMap = new HashMap<>();
+    private static SwerveAutoBuilder builder;
     private static boolean isRedAlliance = DriverStation.getAlliance() == Alliance.Red;
+
+    public static void init(DrivetrainSubsystem drivetrain) {
+
+        builder = new SwerveAutoBuilder(
+                () -> drivetrain.getPose(),
+                (pose) -> drivetrain.resetPose(pose),
+                AUTO_TRANSLATION_PID_CONSTANTS,
+                AUTO_ROTATION_PID_CONSTANTS,
+                (chassisSpeeds) -> drivetrain.setTargetChassisVelocity(chassisSpeeds),
+                eventMap,
+                isRedAlliance,
+                drivetrain);
+    }
 
     public static Command createNullAuto() {
         return null;
     }
     
-    public static Command createTestAuto(DrivetrainSubsystem drivetrain) {
+    public static Command createTestAuto() {
 
         PathPlannerTrajectory path = PathPlanner.loadPath("TestPath", pathConstraints);
-        HashMap<String, Command> eventMap = new HashMap<>();
-
-        SwerveAutoBuilder builder = new SwerveAutoBuilder(
-                () -> drivetrain.getPose(),
-                (pose) -> drivetrain.resetPose(pose),
-                AUTO_TRANSLATION_PID_CONSTANTS,
-                AUTO_ROTATION_PID_CONSTANTS,
-                (chassisSpeeds) -> drivetrain.setTargetChassisVelocity(chassisSpeeds),
-                eventMap,
-                isRedAlliance,
-                drivetrain);
 
         return new InstantCommand(() -> Logger.getInstance().recordOutput("Drivetrain/Trajectory", path)).andThen(builder.fullAuto(path));
     }
 
-    public static Command createRotationTestAuto(DrivetrainSubsystem drivetrain) {
+    public static Command createRotationTestAuto() {
 
         PathPlannerTrajectory path = PathPlanner.loadPath("RotationTestPath", pathConstraints);
-        HashMap<String, Command> eventMap = new HashMap<>();
 
-        SwerveAutoBuilder builder = new SwerveAutoBuilder(
-                () -> drivetrain.getPose(),
-                (pose) -> drivetrain.resetPose(pose),
-                AUTO_TRANSLATION_PID_CONSTANTS,
-                AUTO_ROTATION_PID_CONSTANTS,
-                (chassisSpeeds) -> drivetrain.setTargetChassisVelocity(chassisSpeeds),
-                eventMap,
-                isRedAlliance,
-                drivetrain);
-
-                return new InstantCommand(() -> Logger.getInstance().recordOutput("Drivetrain/Trajectory", path)).andThen(builder.fullAuto(path));
+        return new InstantCommand(() -> Logger.getInstance().recordOutput("Drivetrain/Trajectory", path)).andThen(builder.fullAuto(path));
     }
 
-    public static Command createDiagonalTestAuto(DrivetrainSubsystem drivetrain) {
+    public static Command createDiagonalTestAuto() {
 
         PathPlannerTrajectory path = PathPlanner.loadPath("DiagonalTestPath", pathConstraints);
-        HashMap<String, Command> eventMap = new HashMap<>();
 
-        SwerveAutoBuilder builder = new SwerveAutoBuilder(
-                () -> drivetrain.getPose(),
-                (pose) -> drivetrain.resetPose(pose),
-                AUTO_TRANSLATION_PID_CONSTANTS,
-                AUTO_ROTATION_PID_CONSTANTS,
-                (chassisSpeeds) -> drivetrain.setTargetChassisVelocity(chassisSpeeds),
-                eventMap,
-                isRedAlliance,
-                drivetrain);
-
-                return new InstantCommand(() -> Logger.getInstance().recordOutput("Drivetrain/Trajectory", path)).andThen(builder.fullAuto(path));
+        return new InstantCommand(() -> Logger.getInstance().recordOutput("Drivetrain/Trajectory", path)).andThen(builder.fullAuto(path));
     }
 
-    public static Command createStraightTestAuto(DrivetrainSubsystem drivetrain) {
+    public static Command createStraightTestAuto() {
 
         PathPlannerTrajectory path = PathPlanner.loadPath("StraightTestPath", pathConstraints);
-        HashMap<String, Command> eventMap = new HashMap<>();
 
-        SwerveAutoBuilder builder = new SwerveAutoBuilder(
-                () -> drivetrain.getPose(),
-                (pose) -> drivetrain.resetPose(pose),
-                AUTO_TRANSLATION_PID_CONSTANTS,
-                AUTO_ROTATION_PID_CONSTANTS,
-                (chassisSpeeds) -> drivetrain.setTargetChassisVelocity(chassisSpeeds),
-                eventMap,
-                isRedAlliance,
-                drivetrain);
-
-                return new InstantCommand(() -> Logger.getInstance().recordOutput("Drivetrain/Trajectory", path)).andThen(builder.fullAuto(path));
+        return new InstantCommand(() -> Logger.getInstance().recordOutput("Drivetrain/Trajectory", path)).andThen(builder.fullAuto(path));
     }
 
-    public static Command createRotationTester(DrivetrainSubsystem drivetrain) {
+    public static Command createRotationTester() {
 
         PathPlannerTrajectory path = PathPlanner.loadPath("RotationTester", pathConstraints);
-        HashMap<String, Command> eventMap = new HashMap<>();
 
-        SwerveAutoBuilder builder = new SwerveAutoBuilder(
-                () -> drivetrain.getPose(),
-                (pose) -> drivetrain.resetPose(pose),
-                AUTO_TRANSLATION_PID_CONSTANTS,
-                AUTO_ROTATION_PID_CONSTANTS,
-                (chassisSpeeds) -> drivetrain.setTargetChassisVelocity(chassisSpeeds),
-                eventMap,
-                isRedAlliance,
-                drivetrain);
-
-                return new InstantCommand(() -> Logger.getInstance().recordOutput("Drivetrain/Trajectory", path)).andThen(builder.fullAuto(path));
+        return new InstantCommand(() -> Logger.getInstance().recordOutput("Drivetrain/Trajectory", path)).andThen(builder.fullAuto(path));
     }
 
     // Default constructor that just throws an exception if you attempt to create an
