6df129d9e6bcad1321805a4e3f43b37b6d8bc007
Richard Elmore <richardelmore@live.com>
2023-02-23 00:59:52 -0800

    Get path mirroring working and cleanup code

--- SoftwareBot/src/main/java/frc/robot/commands/AutoCommandFactory.java
+++ SoftwareBot/src/main/java/frc/robot/commands/AutoCommandFactory.java
@@ -3,18 +3,11 @@ package frc.robot.commands;
 
 import java.util.HashMap;
 
-import org.littletonrobotics.junction.Logger;
-
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
-import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.auto.PIDConstants;
 import com.pathplanner.lib.auto.SwerveAutoBuilder;
-
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.drivetrain.DrivetrainSubsystem;
 
 public final class AutoCommandFactory {
@@ -25,7 +18,6 @@ public final class AutoCommandFactory {
 
     private static HashMap<String, Command> eventMap = new HashMap<>();
     private static SwerveAutoBuilder builder;
-    private static boolean isRedAlliance = DriverStation.getAlliance() == Alliance.Red;
 
     public static void init(DrivetrainSubsystem drivetrain) {
 
@@ -36,7 +28,7 @@ public final class AutoCommandFactory {
                 AUTO_ROTATION_PID_CONSTANTS,
                 (chassisSpeeds) -> drivetrain.setTargetChassisVelocity(chassisSpeeds),
                 eventMap,
-                isRedAlliance,
+                true,
                 drivetrain);
     }
 
@@ -46,37 +38,27 @@ public final class AutoCommandFactory {
     
     public static Command createTestAuto() {
 
-        PathPlannerTrajectory path = PathPlanner.loadPath("TestPath", pathConstraints);
-
-        return new InstantCommand(() -> Logger.getInstance().recordOutput("Drivetrain/Trajectory", path)).andThen(builder.fullAuto(path));
+        return builder.fullAuto(PathPlanner.loadPath("TestPath", pathConstraints));
     }
 
     public static Command createRotationTestAuto() {
 
-        PathPlannerTrajectory path = PathPlanner.loadPath("RotationTestPath", pathConstraints);
-
-        return new InstantCommand(() -> Logger.getInstance().recordOutput("Drivetrain/Trajectory", path)).andThen(builder.fullAuto(path));
+        return builder.fullAuto(PathPlanner.loadPath("RotationTestPath", pathConstraints));
     }
 
     public static Command createDiagonalTestAuto() {
 
-        PathPlannerTrajectory path = PathPlanner.loadPath("DiagonalTestPath", pathConstraints);
-
-        return new InstantCommand(() -> Logger.getInstance().recordOutput("Drivetrain/Trajectory", path)).andThen(builder.fullAuto(path));
+        return builder.fullAuto(PathPlanner.loadPath("DiagonalTestPath", pathConstraints));
     }
 
     public static Command createStraightTestAuto() {
 
-        PathPlannerTrajectory path = PathPlanner.loadPath("StraightTestPath", pathConstraints);
-
-        return new InstantCommand(() -> Logger.getInstance().recordOutput("Drivetrain/Trajectory", path)).andThen(builder.fullAuto(path));
+        return builder.fullAuto(PathPlanner.loadPath("StraightTestPath", pathConstraints));
     }
 
     public static Command createRotationTester() {
 
-        PathPlannerTrajectory path = PathPlanner.loadPath("RotationTester", pathConstraints);
-
-        return new InstantCommand(() -> Logger.getInstance().recordOutput("Drivetrain/Trajectory", path)).andThen(builder.fullAuto(path));
+        return builder.fullAuto(PathPlanner.loadPath("RotationTester", pathConstraints));
     }
 
     // Default constructor that just throws an exception if you attempt to create an
