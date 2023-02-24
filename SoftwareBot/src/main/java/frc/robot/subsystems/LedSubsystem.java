e04efa8cd247e302313be911da0ae72391c021aa
Richard Elmore <richardelmore@users.noreply.github.com>
2023-02-23 19:33:38 -0800

    Add framework of game logic class and LedSubsystem

---
+++ SoftwareBot/src/main/java/frc/robot/subsystems/LedSubsystem.java
@@ -0,0 +1,19 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class LedSubsystem extends SubsystemBase {
+
+    public LedSubsystem() {
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+
+    public void setColor() {
+
+    }  
+}
---
+++ SoftwareBot/src/main/java/frc/robot/utility/GameDataHelper.java
@@ -0,0 +1,5 @@
+package frc.robot.utility;
+
+public class GameDataHelper {
+    
+}
