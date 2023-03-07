74ef6ba199baf3029b9d7c7436e927ad387c3ef8
Will Tomasini <34931592+willTomasini@users.noreply.github.com>
2023-03-06 20:39:56 -0800

    Modify IntakeFeedCommand

--- SoftwareBot/src/main/java/frc/robot/commands/IntakeFeedCommand.java
+++ SoftwareBot/src/main/java/frc/robot/commands/IntakeFeedCommand.java
@@ -6,10 +6,12 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.intake.IntakeSubsystem;
+import frc.robot.utility.GameModeUtil;
+import frc.robot.utility.GamePiece;
 
 public class IntakeFeedCommand extends CommandBase {
     private final IntakeSubsystem intake;
-    private final double volts;
+    private double volts;
 
     /** Creates a new IntakeFeedCommand. */
     public IntakeFeedCommand(IntakeSubsystem intake, double volts) {
@@ -22,6 +24,9 @@ public class IntakeFeedCommand extends CommandBase {
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
+        if (GameModeUtil.get() == GamePiece.CONE) {
+            volts = -volts;
+        }
         intake.setVoltage(volts);
     }
 
