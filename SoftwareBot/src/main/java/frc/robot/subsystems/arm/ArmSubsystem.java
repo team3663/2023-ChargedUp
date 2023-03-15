bcff94db672d021e9dd833b5f1d0711faf922659
Richard Elmore <richardelmore@users.noreply.github.com>
2023-03-14 19:43:14 -0700

    Fix limits for new intake

--- SoftwareBot/src/main/java/frc/robot/subsystems/arm/ArmSubsystem.java
+++ SoftwareBot/src/main/java/frc/robot/subsystems/arm/ArmSubsystem.java
@@ -27,7 +27,7 @@ public class ArmSubsystem extends SubsystemBase {
     private static final double ELBOW_MIN_ANGLE_RAD = Units.degreesToRadians(-173);
     private static final double ELBOW_MAX_ANGLE_RAD = Units.degreesToRadians(-3);
     private static final double WRIST_MIN_ANGLE_RAD = Units.degreesToRadians(-116);
-    private static final double WRIST_MAX_ANGLE_RAD = Units.degreesToRadians(127);
+    private static final double WRIST_MAX_ANGLE_RAD = Units.degreesToRadians(120);
 
     // While the elbow is inside the danger zone, the wrist must stay within its safety zone to prevent damage.
     private static final double ELBOW_DANGER_ZONE_RAD = Units.degreesToRadians(-130);
