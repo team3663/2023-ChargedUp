6de4374f664c6ec4cce89470347762a7ab8a768b
Joshua Elmore <boyscout574@outlook.com>
2023-03-21 00:50:57 -0700

    Implament Rambow

--- src/main/java/frc/robot/subsystems/LedSubsystem.java
+++ src/main/java/frc/robot/subsystems/LedSubsystem.java
@@ -1,17 +1,27 @@
 package frc.robot.subsystems;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 
 public class LedSubsystem extends SubsystemBase {
 
     private final double pwmRateHz = 100.0;
 
+    private final int step = 100;
+
     private DigitalOutput red;
     private DigitalOutput green;
     private DigitalOutput blue;
+    private Color8Bit curentColor;
+
+    private Color8Bit[] colores;
+    private int thruRambow;
 
     public LedSubsystem(int redChannel, int greenChannel, int blueChannel) {
 
@@ -24,12 +34,49 @@ public class LedSubsystem extends SubsystemBase {
 
         blue = new DigitalOutput(blueChannel);
         blue.enablePWM(0);
+
+        createColores();
+    
+    }
+
+    @Override
+    public void periodic() {
+        if (this.getCurrentCommand() == null) {
+            setColor(colores[thruRambow]);
+        }
+
+        long[] RGB = {curentColor.red, curentColor.green, curentColor.blue};
+
+        Logger.getInstance().recordOutput("LED/RGBvalues", RGB);
     }
 
     public void setColor(Color8Bit color) {
 
-        red.updateDutyCycle((double)color.red / 255.0);
-        green.updateDutyCycle((double)color.green / 255.0);
-        blue.updateDutyCycle((double)color.blue / 255.0);
-    } 
+        curentColor = color;
+
+        red.updateDutyCycle((double) color.red / 255.0);
+        green.updateDutyCycle((double) color.green / 255.0);
+        blue.updateDutyCycle((double) color.blue / 255.0);
+    }
+
+    private void createColores() {
+        List<Color8Bit> colors = new ArrayList<Color8Bit>();
+
+        for (int r = 0; r < 100; r++)
+            colors.add(new Color8Bit(r * 255 / step, 255, 0));
+        for (int g = 100; g > 0; g--)
+            colors.add(new Color8Bit(255, g * 255 / step, 0));
+        for (int b = 0; b < 100; b++)
+            colors.add(new Color8Bit(255, 0, b * 255 / step));
+        for (int r = 100; r > 0; r--)
+            colors.add(new Color8Bit(r * 255 / step, 0, 255));
+        for (int g = 0; g < 100; g++)
+            colors.add(new Color8Bit(0, g * 255 / step, 255));
+        for (int b = 100; b > 0; b--)
+            colors.add(new Color8Bit(0, 255, b * 255 / step));
+        colors.add(new Color8Bit(0, 255, 0));
+
+        colores = colors.toArray(new Color8Bit[colors.size()]);
+    }
+
 }
