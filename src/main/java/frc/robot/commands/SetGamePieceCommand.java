c5a88f5aa8ac8951bc20d983505a4af6117b6a15
Richard Elmore <richardelmore@live.com>
2023-03-21 02:02:30 -0700

    Set pickup location based on game piece

--- src/main/java/frc/robot/commands/SetGamePieceCommand.java
+++ src/main/java/frc/robot/commands/SetGamePieceCommand.java
@@ -3,18 +3,25 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.utility.GameMode;
 import frc.robot.utility.GameMode.GamePiece;
+import frc.robot.utility.GameMode.PickupLocation;
 
 public class SetGamePieceCommand extends CommandBase {
 
   private GamePiece piece;
+  private PickupLocation pickup;
 
   public SetGamePieceCommand(GamePiece piece) {
     this.piece = piece;
+
+    // At the moment we only pickup cubes from the floor and cones from the double station
+    // So we can choose the correct pickup location here.
+    pickup = piece == GamePiece.CUBE ? PickupLocation.FLOOR : PickupLocation.DOUBLE_STATION;
   }
 
   @Override
   public void initialize() {
     GameMode.setGamePiece(piece);
+    GameMode.setPickupLocation(pickup);
   }
 
   @Override
