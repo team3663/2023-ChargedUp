d5d0ed68e41f2b31c18b29d4a27d4f2e0350c0e2
Will Tomasini <34931592+willTomasini@users.noreply.github.com>
2023-03-24 18:24:58 -0700

    Tune PlaceCommand

--- src/main/java/frc/robot/commands/PlaceCommand.java
+++ src/main/java/frc/robot/commands/PlaceCommand.java
@@ -1,32 +1,25 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.arm.ArmPoseLibrary;
 import frc.robot.subsystems.arm.ArmSubsystem;
 import frc.robot.subsystems.arm.ArmPoseLibrary.ArmPoseID;
-import frc.robot.subsystems.intake.IntakeSubsystem;
 import frc.robot.utility.GameMode;
-import frc.robot.utility.GameMode.GamePiece;
 
 public class PlaceCommand extends CommandBase {
     
     private final ArmSubsystem arm;
-    private final IntakeSubsystem intake;
 
-    /** Creates a new ScoreCommand. */
-    public PlaceCommand(ArmSubsystem arm, IntakeSubsystem intake) {
+    public PlaceCommand(ArmSubsystem arm) {
         this.arm = arm;
-        this.intake = intake;
     }
 
-    // Called when the command is initially scheduled.
     @Override
     public void initialize() {
         switch (GameMode.getScoringPosition()) {
+            case LOW:
+                arm.setTargetPose(ArmPoseLibrary.get(ArmPoseID.SCORE_LOW));
+                break;
             case MIDDLE:
                 arm.setTargetPose(ArmPoseLibrary.get(ArmPoseID.SCORE_MED));
                 break;
@@ -34,30 +27,18 @@ public class PlaceCommand extends CommandBase {
                 arm.setTargetPose(ArmPoseLibrary.get(ArmPoseID.SCORE_HI));
                 break;
             default:
-                arm.setTargetPose(ArmPoseLibrary.get(ArmPoseID.SCORE_LOW));
-                break;
+                System.out.println("Error: Invalid scoring position");
         }
     }
 
-    // Called every time the scheduler runs while the command is scheduled.
     @Override
-    public void execute() {
-        if (arm.atTargetPose()) {
-            if (GameMode.getGamePiece() == GamePiece.CONE) {
-                intake.setPower(-1);
-            } else {
-                intake.setPower(-0.5);
-            }
-        }
-    }
+    public void execute() {}
 
-    // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
         arm.setTargetPose(ArmPoseLibrary.get(ArmPoseID.STOWED));
     }
 
-    // Returns true when the command should end.
     @Override
     public boolean isFinished() {
         return false;
