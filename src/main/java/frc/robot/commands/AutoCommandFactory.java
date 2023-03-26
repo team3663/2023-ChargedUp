e955b67c1bcb51b49f58cd629311db2446e6be93
Richard Elmore <richardelmore@live.com>
2023-03-26 14:33:53 -0700

    Remove unused import

--- src/main/java/frc/robot/commands/AutoCommandFactory.java
+++ src/main/java/frc/robot/commands/AutoCommandFactory.java
@@ -12,7 +12,6 @@ import com.pathplanner.lib.auto.SwerveAutoBuilder;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.arm.ArmSubsystem;
 import frc.robot.subsystems.arm.ArmPoseLibrary.ArmPoseID;
 import frc.robot.subsystems.drivetrain.DrivetrainSubsystem;
